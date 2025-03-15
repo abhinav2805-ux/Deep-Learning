a712de6792389df97ec6f01d6b8297e0dd5b3751
Abhinav Gupta <gupta380189@gmail.com>
2025-03-15 20:29:15 +0530

    first commit

---
+++ Natural Language Processing/Mastering String &  ASCII/.ipynb_checkpoints/nlp-checkpoint.py
@@ -0,0 +1,20 @@
+def capitalize(text):
+    return text.capitalize()
+
+def to_upper(text):
+    return text.upper()
+
+def to_lower(text):
+    return text.lower()
+
+def is_title(text):
+    return text.istitle()
+
+def is_digit(text):
+    return text.isdigit()
+
+def is_alpha(text):
+    return text.isalpha()
+
+def is_numeric(text):
+    return text.isnumeric()
